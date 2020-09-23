1181c9dfc66fa9a2858cee5f4c904324e24bf8fe
wyx180414424 <204465553@qq.com>
2020-09-23 13:44:54 +0800

    3

---
+++ Project4/main.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+int main()
+{
+	cv::Mat srcMat = imread("E:/ͼƬ/keda.jpg");
+	uchar threshold = 100;
+	int height = srcMat.rows;
+	int width = srcMat.cols;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold) average = 255;
+			else average = 0;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+
+		}
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
+}
